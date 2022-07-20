a1fa3e9be3ffa10f79f3ae0242b15372e2df61a3
Gabriel Senna <usasenna@gmail.com>
2022-07-20 14:57:09 -0300

    footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,17 +1,36 @@
 import './styles/Footer.css';
-import React from 'react'
+import React from 'react';
 
-// import logo from './logo-lateral.png'
+import logoFooter from './midia/logo-circulo.svg';
+import faa from './midia/faa-certified.svg';
 
 
 function Footer() {
 
     return (
-      <div className='Footer' >
-       <p>oi</p>
+    <div className='main'>
+
+      <div className='footer' >
+        <div className='center-footer' >
+          <img alt="Soflair aviation miami" src={logoFooter}  className='logo-footer' />
+        </div>
+        <div className='center-footer' >
+          <img alt="FAA certification" src={faa}  className='logo-footer' />
+        </div>
+        <div >
+          <p className='footer-text'>Soflair Aviations in Miami is approved</p>
+          <p className='footer-text'>by Federal Aviation Administration</p>
+        </div>
+        
       </div>
-    );
-  }
+        <div className='annoucement-footer'>
+          <p className='footer-text cor-principal' >© 2022 Soflair Aviations. All rights reserved.</p>
+          <p className='footer-text rem06 cor-principal'>This site is not a part of the Facebook website or Facebook Inc. Additionally, This site is NOT endorsed by Facebook in any way. FACEBOOK is a trademark of META PLATFORMS, Inc.</p>
+      </div>
+
+    </div>
+  );
+}
   
   export default Footer;
   
