66060b96c90e9ff1d0a7b6e249e3912e5e77712a
Gabriel Senna <usasenna@gmail.com>
2022-07-20 14:31:22 -0300

    header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,38 +1,24 @@
 import './styles/Header.css';
 import React from 'react'
 
-// import logo from './logo-lateral.png'
+import logo from './midia/logo header.svg'
+import menu from './midia/menu.svg'
 
 
 function Header() {
-
-  // Evento de scroll dispara a seguinte função:
-  // const target = document.querySelector('.header-logo')
-  // const target2 = document.querySelector('.tab2')
-  // window.addEventListener('scroll', function(){
-  //   if(window.pageYOffset > 30){
-  //     target.classList.toggle("header-animate", true)
-  //     target2.classList.toggle("top-40", true)
-  //   } else{
-  //     target.classList.toggle("header-animate", false)
-  //     target2.classList.toggle("top-40", false) }
-  // })
-
     return (
       <div className='header' >
-        {/* input serve para validar se o announcemente aparece ou n */}
-        <input type='checkbox' name='announcement-close' id='check-announcement' className='input-slide' />
-        <label className='close-announcement' htmlFor='check-announcement'>x</label>
         <div className='announcement'>
-          <span className='announcement-text'>Airplane tours in Miami. Book now <b>!</b></span>
+          <span className='announcement-text'>Airplane tours in Miami. Book now!</span>
           
         </div>
 
-        <div className='branco'>
-          <div  className='tab header-logo' >
+        <div>
+          <div  className='header-logo' >
             <a href='./' >
-              {/* <img alt="Soflair aviation miami" src={logo}  className='center-top-logo' /> */}
+              <img alt="Soflair aviation miami" src={logo}  className='center-top-logo' />
             </a>
+            <img alt='menu' src={menu} className='menu'/>
           </div>
             
         </div>
